21fbb20a3585054399a5b004fcf03ef2f020ef13
Syifaul Qolbi <syifaul@pop-os.localdomain>
2022-12-23 22:16:34 +0700

    add template

---
+++ website/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+
+import Header from "../components/header/Header";
+import HeroSlider from "../components/hero-slider/HeroSlider";
+import PopularMenu from "../components/popular-menu/PopularMenu";
+import ChooseUs from "../components/choose-us/ChooseUs";
+import MenuPack from "../components/menu-pack/MenuPack";
+import Testimonials from "../components/testimonials/Testimonials";
+import Footer from "../components/footer/Footer";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Header />
+      <HeroSlider />
+      <PopularMenu />
+      <ChooseUs />
+      <MenuPack />
+      <Testimonials />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Home;
