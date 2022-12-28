57373319b2952235859c9d4a911753ba0fda3384
unknown <maretanur197@gmail.com>
2022-12-28 14:41:50 +0700

    feat: fetching about us

--- website/src/api.js
+++ website/src/api.js
@@ -1,8 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_BASEURL;
 
 export const getMenuList = async () => {
-    const menu = await axios.get(`${baseUrl}/api/menus/`);
-    return menu.data;
+  const menu = await axios.get(`${baseUrl}/api/menus/`);
+  return menu.data;
+};
+
+export const getProfile = async () => {
+  const profile = await axios.get(`${baseUrl}/api/profile/`);
+  return profile.data;
 };
--- website/src/components/footer/Footer.jsx
+++ website/src/components/footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { getProfile } from "../../api";
 import "./footer.css";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { useEffect } from "react";
 
 const footerLinks = [
   {
@@ -23,6 +25,15 @@ const footerLinks = [
 ];
 
 const Footer = () => {
+  const [profile, setProfile] = React.useState([]);
+
+  React.useEffect(() => {
+    getProfile().then((result) => {
+      setProfile(result);
+    });
+  }, []);
+
+
   return (
     <footer className="footer">
       <div className="footer__top">
@@ -34,11 +45,14 @@ const Footer = () => {
                   <span>
                     <i class="ri-restaurant-2-line"></i>
                   </span>{" "}
-                  Chef Food
+                  {profile.map((item) => (
+                    <span>{item.name}</span>
+                  ))}
                 </h2>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-                  facere eligendi laboriosam id exercitationem tempore.
+                  {profile.map((item) => (
+                    <span>{item.about_us}</span>
+                  ))}
                 </p>
               </div>
             </Col>
@@ -48,32 +62,31 @@ const Footer = () => {
 
             </Col>
 
-            <Col lg="2" md="4" sm="6">
-              <h5 className="footer__link-title">Quick Links</h5>
-
-              <ListGroup>
-                {footerLinks.map((item, index) => (
-                  <ListGroupItem key={index} className="link__item">
-                    <a href={item.url}>{item.display}</a>
-                  </ListGroupItem>
-                ))}
-              </ListGroup>
-            </Col>
+        
 
             <Col lg="3" md="4" sm="6">
               <h5 className="footer__link-title">Contact</h5>
 
               <ListGroup>
                 <ListGroupItem className="link__item d-flex align-items-center gap-3">
-                  <i class="ri-map-pin-line"></i> Sylhet, Bangladesh{" "}
+                  <i class="ri-map-pin-line"></i> 
+                  {profile.map((item) => (
+                    <span>{item.address}</span>
+                  ))}
                 </ListGroupItem>
 
                 <ListGroupItem className="link__item d-flex align-items-center gap-3">
-                  <i class="ri-mail-line"></i> example@gmail.com{" "}
+                  <i class="ri-mail-line"></i> 
+                  {profile.map((item) => (
+                    <span>{item.email}</span>
+                  ))}
                 </ListGroupItem>
 
                 <ListGroupItem className="link__item d-flex align-items-center gap-3">
-                  <i class="ri-phone-line"></i> +880 123 456 7890
+                  <i class="ri-phone-line"></i> 
+                  {profile.map((item) => (
+                    <span>{item.phone}</span>
+                  ))}
                 </ListGroupItem>
               </ListGroup>
             </Col>
@@ -85,7 +98,7 @@ const Footer = () => {
         <Container>
           <Row>
             <Col lg="12">
-              <p>copyright 2022, developed by muhib. All rights reserved</p>
+              <p>copyright 2022, developed by Kelompok 3. All rights reserved</p>
             </Col>
           </Row>
         </Container>
