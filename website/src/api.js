21cc50e1592228373fbd426eeab865b3c8c1da11
Doni Wahyu S <doniwahyu56@gmail.com>
2022-12-28 15:59:52 +0700

    add route FAQ

--- website/src/api.js
+++ website/src/api.js
@@ -6,3 +6,8 @@ export const getMenuList = async () => {
     const menu = await axios.get(`${baseUrl}/api/menus/`);
     return menu.data;
 };
+
+export const getFaq = async () => {
+    const faq = await axios.get(`${baseUrl}/api/faq/`);
+    return faq.data;
+}
