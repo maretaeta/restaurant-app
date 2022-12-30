91a5db8bc387d68fe1dbd732522c4f1a32d2029c
unknown <maretanur197@gmail.com>
2022-12-30 15:07:59 +0700

    profile baru

--- website/src/api.js
+++ website/src/api.js
@@ -3,18 +3,18 @@ import axios from "axios";
 const baseUrl = process.env.REACT_APP_BASEURL;
 
 export const getMenuList = async () => {
-    const menu = await axios.get(`${baseUrl}/api/menus/`);
-    return menu.data;
+  const menu = await axios.get(`${baseUrl}/api/menus/`);
+  return menu.data;
 };
 
 export const getMenuCategories = async () => {
-    const menuCategories = await axios.get(`${baseUrl}/api/menus/categories/`);
-    return menuCategories.data;
-}
+  const menuCategories = await axios.get(`${baseUrl}/api/menus/categories/`);
+  return menuCategories.data;
+};
 export const getFaq = async () => {
-    const faq = await axios.get(`${baseUrl}/api/faq/`);
-    return faq.data;
-}
+  const faq = await axios.get(`${baseUrl}/api/faq/`);
+  return faq.data;
+};
 
 export const getProfile = async () => {
   const profile = await axios.get(`${baseUrl}/api/profile/`);
@@ -22,6 +22,6 @@ export const getProfile = async () => {
 };
 
 export const getOpening = async () => {
-    const opening = await axios.get(`${baseUrl}/api/opening-hour/`);
-    return opening.data;
-}
+  const opening = await axios.get(`${baseUrl}/api/opening-hour/`);
+  return opening.data;
+};
--- website/src/components/footer/Footer.jsx
+++ website/src/components/footer/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
             <Col lg="3" md="4" sm="6">
               <h5 className="footer__link-title"></h5>
 
-            </Col>j
+            </Col>
 
         
 
@@ -98,7 +98,7 @@ const Footer = () => {
         <Container>
           <Row>
             <Col lg="12">
-              <p>copyright 2022, developed by Kelompok 3. All rights reserved</p>
+              <p>developed by Kelompok 3.</p>
             </Col>
           </Row>
         </Container>
