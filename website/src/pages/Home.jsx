4a16a03fd5f6b033e8ac1c3dbb31b3a2324de966
Syifaul Qolbi <syifaul@pop-os.localdomain>
2023-01-02 08:48:20 +0700

    feat : comment unUsed import

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React, { Fragment } from "react";
 
 import Header from "../components/header/Header";
 import HeroSlider from "../components/hero-slider/HeroSlider";
-import Menu from "../components/popular-menu/PopularMenu";
-import ChooseUs from "../components/choose-us/ChooseUs";
+// import Menu from "../components/popular-menu/PopularMenu";
+// import ChooseUs from "../components/choose-us/ChooseUs";
 import MenuPack from "../components/menu-pack/MenuPack";
 import Testimonials from "../components/testimonials/Testimonials";
 import Footer from "../components/footer/Footer";
