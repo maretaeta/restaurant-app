8218e68eea2914cdee4d9aa07fcb72934968b577
Syifaul Qolbi <syifaul@pop-os.localdomain>
2023-01-03 12:44:19 +0700

    fix allowed host error

--- server/server/settings.py
+++ server/server/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9+ndv47%ub&^a4y91mflepyc3je96tit@@6szr356rqbw-98db
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://eat-more.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
